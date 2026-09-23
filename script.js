b09ffa70c86a5ccc693d4f89512fb51eaae00028
Julia Salinero <jus628@lehigh.edu>
2026-09-23 17:42:25 -0400

    Fixed feedback given on post it note: responsiveness btwn tablet and mobile, scroll speed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("wheel", function(event) {
   event.preventDefault();
 
   window.scrollBy({
-    top: event.deltaY * 0.5,
-    behavior: "smooth"
+    top: event.deltaY * 0.10,
   });
+
 }, { passive: false });
